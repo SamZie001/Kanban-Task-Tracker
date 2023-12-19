0c663f36f3cca91a49369ac45ac18cb659c8d9f5
SamZie001 <samzie001@gmail.com>
2023-12-19 19:19:30 +0100

    fix: removed localstorage for produvtion purposes

--- app/context/userContext.tsx
+++ app/context/userContext.tsx
@@ -4,22 +4,22 @@ import { createContext, useContext, useState, useEffect } from "react";
 export const UserContext = createContext<any>(null);
 
 export function UserProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<{} | null>({ _id: "", username: "" });
+  const [user, setUser] = useState<{} | null>(null);
 
-  useEffect(() => {
-    const preUser = localStorage.getItem("user");
-    if (preUser) setUser(JSON.parse(preUser));
-    else setUser(null);
-  }, []);
+  // useEffect(() => {
+  //   const preUser = localStorage.getItem("user");
+  //   if (preUser) setUser(JSON.parse(preUser));
+  //   else setUser(null);
+  // }, []);
 
   const loginUser = (newUser: {}) => {
     setUser(newUser);
-    localStorage.setItem("user", JSON.stringify(newUser));
+    // localStorage.setItem("user", JSON.stringify(newUser));
   };
 
   const logoutUser = () => {
     setUser(null);
-    localStorage.clear();
+    // localStorage.clear();
   };
 
   return (
