8d93643fb771258b95affef46487b636ad66696a
Samuel Izevbizua <80903513+SamZie001@users.noreply.github.com>
2025-03-02 21:59:35 +0100

    Update layout.tsx
    Terminate string literal

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const quicksand = Quicksand({
 export const metadata: Metadata = {
   title: "Maiboard",
   description: "Maiboard is a Kanban task board that helps you organize and manage your tasks efficiently.",
-  keywords: ['Next.js', 'Maiboard', 'Kanboard', 'Task Board', 'DnD', 'Drag and Drop', 'Projects', 'Github', 'Open Source]
+  keywords: ['Next.js', 'Maiboard', 'Kanboard', 'Task Board', 'DnD', 'Drag and Drop', 'Projects', 'Github', 'Open Source']
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
